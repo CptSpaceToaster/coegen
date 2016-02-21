36f847e187ec9d270e2219467f6b0d587f1aecf3
CptSpaceToaster <CptSpaceToaster@gmail.com>
2016-02-21 11:53:03 -0500

    Make work

--- coegen/main.py
+++ coegen/main.py
@@ -9,27 +9,38 @@ from PIL import Image
 def main():
     # Parse the CL arguments
     parser = argparse.ArgumentParser(prog=coegen.CLI, description=coegen.DESCRIPTION)
-    parser.add_argument('FILE', nargs='1', default=os.path.join(os.curdir, ''),
-                        help='Path to image file')
+    parser.add_argument('FILE', help='Path to image file')
     parser.add_argument('-o', '--output', dest='output', default=None,
                         help='Output filename')
+    parser.add_argument('-f', '--format', dest='format', default='01x',
+                        help='Pixel format (default="01x"), try "04b"')
     parser.add_argument('-v', '--version', action='version', version=coegen.__version__)
     args = parser.parse_args()
 
-    if not os.path.isfile(args.PATH):
+    if not os.path.isfile(args.FILE):
         print('Error: {0} not found.'.format(args.FILE))
         sys.exit(1)
 
     if args.output:
         outfile = args.output
     else:
-        outfile = os.basename(args.FILE).split()[0] + '.coe'
+        outfile = os.path.basename(args.FILE).split('.')[0] + '.coe'
 
-    I = numpy.array(Image.open(args.FILE), dtype=numpy.int16)
+    print('reading {0}'.format(args.FILE))
+    I = numpy.array(Image.open(args.FILE), dtype=numpy.uint8)
+    print('writing {0}'.format(outfile))
+
+    """
+    iteration happens like this
+      ---------->
+     | 1 2 3 4 5
+     | 6 7 8 9 10
+
+    """
     with open(outfile, 'w') as f:
         f.write('MEMORY_INITIALIZATION_RADIX=2;\n')
         f.write('MEMORY_INITIALIZATION_VECTOR=')
-        for pixel in I:
+        for row in I:
             f.write('\n')
-            f.write(''.join(list(map(lambda c: format(c, '08b'), pixel[0:3]))))
+            f.write(' '.join(map(lambda pixel: ''.join(map(lambda c: format(c >> 4, args.format), pixel[0:3])), row)))
         f.write(';\n')
